91c42bd7d44bf1077199c29ebf8103f9984a94bc
Jonah Riley Huggins <jonahrileyhuggins@gmail.com>
2024-09-06 19:14:42 -0400

    Updates
    - Restructured benchmark utilities
    - yaml files now need to include benchmark compatible models
    - RPPA benchmark is using level4 data and includes IFNG perturbations now.

---
+++ benchmarks/BIM-dependent-ERK-inhibition/README.md
@@ -0,0 +1,21 @@
+# BIM-Dependent Inhibition of ERK signaling
+
+**Benchmark Author(s): Erdem C, Birtwistle MR, Huggins JR**
+
+**Date Created: 01/31/2024**
+
+**Version: 1.0**
+
+## Summary
+
+![1725107304527](../benchmark_utils/visualization/curated-benchmark-visuals/BIM-dependent-ERK-inhibition.png)
+
+"Simulations where either BAD- or BIM-dependent mechanisms were computationally knocked-out predicted BIM to account for most ERK and AKT inhibition effects. Independent experimental data corroborate this prediction; MCF10A cell detachment-induced death, which depends on such survival signaling mechanisms, has been observed to be predominantly controlled by BIM rather than BAD. The model suggests several contributing mechanisms. One reason is expression levels; BAD is lowly expressed compared to anti-apoptotic BCL2 family proteins (~14,000 vs. ~61,000 molecules per cell), and thus is unlikely to exert significant control over cell death. Another is the cascade network structure for BIM regulation. If only AKT is inhibited, phosphorylation by ERK blocks the accumulation of active BIM. If only ERK is inhibited, the amount of BIM in the cell, even if completely unphosphorylated, is largely insufficient to drive cell death in the absence of further strong death signals. However, when both pathways are inhibited, the production and accumulation of active BIM proceeds uninhibited, thus creating a potent apoptotic signal. Furthermore, the inhibition of these activities must be coordinated over relatively long periods of time, with duration of AKT inhibition having enough overlap with that of ERK to cause both the accumulation and activation of BIM."
+
+    - A mechanistic pan-cancer pathway model informed by multi-omics data interprets stochastic cell fate responses to drugs and mitogens [1]
+
+References:
+
+1. Bouhaddou M, Barrette AM, Stern AD, Koch RJ, DiStefano MS, Riesel EA, Santos LC, Tan AL, Mertz AE, Birtwistle MR. A mechanistic pan-cancer pathway model informed by multi-omics data interprets stochastic cell fate responses to drugs and mitogens. PLoS Comput Biol. 2018 Mar 26;14(3):e1005985. doi: 10.1371/journal.pcbi.1005985. Erratum in: PLoS Comput Biol. 2018 Jul 9;14(7):e1006189. doi: 10.1371/journal.pcbi.1006189. PMID: 29579036; PMCID: PMC5886578.
+
+## Benchmark Reports:
--- benchmarks/benchmark_utils/simulation/observable_calc.py
+++ benchmarks/benchmark_utils/simulation/observable_calc.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+script name: Observable_calc.py
+Created on Fri April 24 18:00:00 2024
+Author: Jonah R. Huggins
+
+Description: This script is designed to calculate observable values from 
+             simulation results.
+
+Output: dictionary containing the observables of interest
+
+"""
+#-----------------------Package Import & Defined Arguements-------------------#
+
 import re
 import numpy as np
 import pandas as pd
@@ -5,7 +20,9 @@ from typing import Optional
 
 
 class ObservableCalculator:
-    def __init__(self, yaml_file:str, results_dict: dict, observable_df: pd.DataFrame, measurement_df: pd.DataFrame, model: str):
+    def __init__(self, yaml_file:str, results_dict: dict, 
+                 observable_df: pd.DataFrame, measurement_df: pd.DataFrame, 
+                 model: str):
         """This class is designed to calculate observable values from simulation results.
         input:
             yaml_file: str - path to the YAML file
@@ -100,75 +117,3 @@ class ObservableCalculator:
                 result_dict[condition][f'cell {i}'][f'experiment {observable}']['xoutS'] = condition_data['measurement'].values
         
         return result_dict
-
-
-class CellDeathMetrics:
-    def __init__(self, data, observable_name):
-        """ This is extended functionality for the observable calculator class. 
-        It is designed to calculate different death point metrics for each cell in the simulation results.
-
-        data: dictionary containing the simulation results from ObservableCalculator
-        observable_name: name of the observable to be used to determine time of death
-        """
-        self.data = data
-        self.observable_name = observable_name
-
-    def time_to_death(self):
-        """"Returns the time for each simulated cell death for each condition in the results dictionary
-        
-        output: dictionary containing the times to death for each cell per condition"""
-
-        time_of_death = {}
-        for condition in self.data:
-            time_of_death[condition] = []
-            for cell in self.data[condition]:
-                dead_simulation = np.array(self.data[condition][cell][self.observable_name]['toutS']\
-                                           [self.data[condition][cell][self.observable_name]['xoutS'] > 100.0])
-                if dead_simulation.size > 0:
-                    dead_simulation_times = dead_simulation[0]             
-                    time_of_death[condition].extend(dead_simulation_times.flatten().tolist())
-                else:
-                    time_of_death[condition].append(np.nan)
-        return time_of_death
-    
-    def average_time_to_death(self):   
-        """"Returns the time for the average simulated cell death for each condition in the results dictionary
-        
-        output: dictionary containing the average time to death for each condition"""
-
-        time_of_death = self.time_to_death()
-        for condition, cell_times in self.time_to_death().items():
-            time_of_death[condition] = np.mean([time for time in time_of_death[condition] if time is not None])
-
-        return time_of_death
-
-    def death_ratio(self, percent:Optional[bool] = False):
-        """Returns the ratio of dead cells, should be proceeded by collect_the_dead function
-        time_to_death: dictionary containing the times to death for each cell per condition from the time_to_death function
-
-        output: dictionary containing the ratio of dead cells for each condition"""
-
-        dead_cells = {}
-        for condition, cell_times in self.time_to_death().items():
-            dead_cells[condition] = 0
-            total_cells = len(self.data[condition])
-            dead_cells[condition] += sum(1 for time in cell_times if time is not np.nan)
-            # dead_cells[condition] = [dead_cells[condition] * (1 / total_cells) if total_cells !=0 else None for condition in dead_cells]
-            dead_cells[condition] = dead_cells[condition] / total_cells if total_cells != 0 else None
-            if percent:
-                dead_cells[condition] = dead_cells[condition] * 100
-
-        return dead_cells
-    
-    def alive_ratio(self, percent:Optional[bool] = False):
-        """Returns the ratio of alive cells, should be proceeded by collect_the_dead function
-        
-        output: dictionary containing the ratio of alive cells for each condition"""
-
-        death_ratio = self.death_ratio()
-        if percent == True:
-            alive_ratio = [(1 - x)*100 for x in death_ratio.values()]
-        else:
-            alive_ratio = [(1 - x) for x in death_ratio.values()]
-        # alive_ratio = [(1 - x)*100 for x in death_ratio.values()]
-        return alive_ratio 
