59e85723e09f6483f5cd770d8bbcc12e211e838c
chocolatecharlie <aurore.amrit@umu.se>
2024-11-19 12:32:52 +0100

    Separate files handling from data handling

--- SPARCED/src/utils/data_handling.py
+++ SPARCED/src/utils/data_handling.py
@@ -9,40 +9,7 @@ import pandas as pd
 from pathlib import Path
 import yaml
 
-def append_subfolder(folder: str | os.PathLike, subfolder: str,
-                     abort_on_error: bool=False) -> str | os.PathLike:          
-    """Append a subfolder to a folder path
 
-    Arguments:
-        folder: The folder path.
-        subfolder: The subfolder name.
-        abort_on_error: Abort process when encountering an error.
-
-    Returns:
-        The subfolder's path.
-    """
-
-    folder = Path(folder)
-    try:
-        assert folder.exists()
-    except:
-        print("WARNING: Folder doesn't exist. This is never normal.\nFolder name:{name}."
-              .format(name=folder))
-        if abort_on_error:
-            print("Aborting now.")
-            sys.exit(0)
-
-    subfolder_path = folder / subfolder
-    try:
-        assert subfolder_path.exists()
-    except:
-        print("WARNING: Subfolder doesn't exist yet. This is normal if you are creating a new subfolder.\nSubfolder name: {name}."
-              .format(name=subfolder))
-        if abort_on_error:
-            print("Aborting now.")
-            sys.exit(0)
-
-    return(subfolder_path)
 
 def convert_excel_to_tsv(f_excel: str) -> None:
     """Convert an Excel file to TSV (SPARCED's standard input format)           
---
+++ SPARCED/src/utils/files_handling.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+from pathlib import Path
+
+
+
+def append_subfolder(folder_path: str | os.PathLike, subfolder_name: str) -> str | os.PathLike:
+    """Append a subfolder to a folder path
+
+    Arguments:
+        folder_path: The folder path.
+        subfolder_name: The subfolder name.
+
+    Returns:
+        The subfolder's path.
+    """
+
+    folder = Path(folder_path)
+    check_path_existence(folder)
+
+    subfolder_path = folder / subfolder_name
+    return(subfolder_path)
+
+def check_path_existence(path: str | os.Pathlike) -> None:
+    try:
+        if not path.exists():
+            raise FileNotFoundError(f"Path not found: {path}")
+    except FileNotFoundError as error:
+        print(error)
+        sys.exit(0)
+
