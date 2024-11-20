301684cc341b36c2d794a2ca34f62bdba6efebee
chocolatecharlie <aurore.amrit@umu.se>
2024-11-20 15:18:59 +0100

    Improve SparcedModel.py style

--- SPARCED/src/SparcedModel.py
+++ SPARCED/src/SparcedModel.py
@@ -17,7 +17,9 @@ class InvalidModelName(ValueError):
         self.model_name = model_name
 
     def __str__(self):
-        return(f"SPARCED ERROR: Invalid model name.\nName: {self.model_name}\nError: {self.message}\n")
+        return(f"SPARCED ERROR: Invalid model name.\n \
+                 Name: {self.model_name}\n \
+                 Error: {self.message}\n")
 
 class EmptyModelName(InvalidModelName):
     pass
@@ -27,15 +29,19 @@ class EmptyModelName(InvalidModelName):
 
 class Model:
 
-    def __init__(self, name="SPARCED_standard", models_directory="./../models/", config_name="config.yaml"):
+    def __init__(self, name="SPARCED_standard",
+                 models_directory="./../models/", config_name="config.yaml"):
         # General settings
         self.name = self.set_name(name)
         self.path = append_subfolder(models_directory, self.name)
         self.configuration = self.load_configuration(self.path, config_name)
-        self.data_location = append_subfolder(self.path, self.configuration['location'])
+        self.data_location = append_subfolder(self.path,
+                                              self.configuration['location'])
         check_path_existence(self.data_location)
         # Compilation
-        self.compilation_files = self.load_compilation_files(self.data_location, self.configuration['compilation'])
+        self.compilation_files = self.load_compilation_files(
+                                 self.data_location,
+                                 self.configuration['compilation'])
 
     def load_configuration(self, path: str | os.PathLike, config_name: str):
         config_path = append_subfolder(path, config_name)
@@ -45,29 +51,39 @@ class Model:
         return(configuration)
 
     def load_compilation_files(self, data_location: str | os.PathLike,
-                                     compilation_config) -> dict[str, str | os.PathLike]:
+                                     compilation_config
+                                     ) -> dict[str, str | os.PathLike]:
         """Load compilation input file paths
 
         Note:
             Dictionnary structure is expected to contain:
-            > a 'root' item indicating the location of the compilation input files subfolder
-            > a 'files' item containing a dictionnary of file types and their names
+            > a 'root' item indicating the location of the compilation
+              input files subfolder
+            > a 'files' item containing a dictionnary of file types and
+              their names
 
         Arguments:
-            data_location: The path towards the location of the model's data.
-            compilation_config: A dictionnary structured as stated in the __Note__ section.
+            data_location: The path towards the location of the model's
+                           data.
+            compilation_config: A dictionnary structured as stated in
+                                the __Note__ section.
 
         Returns:
-            A dictionnary structured as key: input file type / value: input file path.
+            A dictionnary structured as key: input file type / value:
+            input file path.
         """
 
-        compilation_data_location = append_subfolder(self.data_location, compilation_config['root'])
+        compilation_data_location = append_subfolder(self.data_location,
+                                                     compilation_config['root'])
         check_path_existence(compilation_data_location)
         # TODO: adjust for multiple files
         for file_type, file_name in compilation_config['files'].items():
             if file_name != None:
-                compilation_config['files'][file_type] = append_subfolder(compilation_data_location, file_name)
-                # Do not check existence of files that will be created upon compilation
+                compilation_config['files'][file_type] = append_subfolder(
+                                                         compilation_data_location,
+                                                         file_name)
+                # Do not check existence of files that will be created upon
+                # compilation
                 if file_type != "output_parameters":
                     check_path_existence(compilation_config['files'][file_type])
         return(compilation_config['files'])
@@ -76,11 +92,13 @@ class Model:
         """Sanitize a name
 
         Note:
-            As Antimony cannot handle the dash ('-') character as a model name,
-            any occurence of this character is replaced by an underscore ('_').
+            As Antimony cannot handle the dash ('-') character as a
+            model name, any occurence of this character is replaced by
+            an underscore ('_').
 
         Warning:
-            This might result into the breakage of some further functionalities.
+            This might result into the breakage of some further
+            functionalities.
 
         Arguments:
             name: The name.
