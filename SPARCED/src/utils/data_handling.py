1e669f94898b286ef675b90deb2760b9e8fc36ec
chocolatecharlie <aurore.amrit@umu.se>
2024-11-21 10:44:42 +0100

    Clean data utils

--- SPARCED/src/utils/data_handling.py
+++ SPARCED/src/utils/data_handling.py
@@ -1,24 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-import sys
-
 import numpy as np
 import pandas as pd
-from pathlib import Path
-import yaml
-
 
 
 def convert_excel_to_tsv(f_excel: str) -> None:
     """Convert an Excel file to TSV (SPARCED's standard input format)           
 
-    This function creates a new .txt file at the same location than the passed  
-    Excel file.
+    This function creates a new .txt file at the same location than the
+    passed Excel file.
 
     Warning:
-        This is some old code written four years ago, it hasn't been tested since.
+        This is some old code written four years ago, it hasn't been
+        tested since.
 
     Arguments:
         f_excel: The Excel sheet path.
@@ -28,7 +23,7 @@ def convert_excel_to_tsv(f_excel: str) -> None:
     """
 
     data = pd.read_excel(f_excel, header=0, index_col=0)
-    data.to_csv((f_excel.split("."))[0] + ".txt", sep="\t") 
+    data.to_csv((f_excel.split("."))[0] + ".txt", sep="\t")
 
 def load_input_data_file(f_input: str | os.PathLike) -> np.ndarray:
     """Load the given input data file
