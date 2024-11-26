274967e7a8da09b0b35e79b319af16658a9a8813
chocolatecharlie <aurore.amrit@umu.se>
2024-11-26 17:31:17 +0100

    Add units to constants

--- SPARCED/src/constants.py
+++ SPARCED/src/constants.py
@@ -7,3 +7,9 @@ ANTIMONY_FILE_PREFIX = "ant_"
 ANTIMONY_FILE_SUFFIX = ".txt"
 ANTIMONY_HEADER = "Erdem et al., Nat Commun 2022"
 
+# UNITS
+UNIT_DEF_NM = "1e-9 mole / litre"
+UNIT_SUBSTANCE = "1e-9 mole"
+UNIT_TIME = "second"
+UNIT_VOLUME = "litre"
+
