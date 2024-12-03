6383587e38295a20c4280fa1401b69225c6a4c25
chocolatecharlie <aurore.amrit@umu.se>
2024-12-03 13:25:58 +0100

    Clean script

--- SPARCED/src/compilation/antimony_scripts/__init__.py
+++ SPARCED/src/compilation/antimony_scripts/__init__.py
@@ -2,13 +2,16 @@
 # -*- coding: utf-8 -*-
 
 
-from compilation.antimony_scripts.definitions import (define_compartments,
-                                                      define_species,
-                                                      define_units)
-from compilation.antimony_scripts.initial_conditions import (set_compartments_ic,
-                                                             set_reactions_ic,
-                                                             set_species_ic)
-from compilation.antimony_scripts.reactions import write_reactions
+from compilation.antimony_scripts.definitions import (
+        define_compartments,
+        define_species,
+        define_units)
+from compilation.antimony_scripts.initial_conditions import (
+        set_compartments_ic,
+        set_reactions_ic,
+        set_species_ic)
+from compilation.antimony_scripts.reactions import (
+        write_reactions)
 
 __all__ = [
             'define_compartments',
