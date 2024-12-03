ebc06e85c21dbf2b9589d6b4d46f8a2a48f562cb
chocolatecharlie <aurore.amrit@umu.se>
2024-12-03 15:47:57 +0100

    Build SPARCED_exp

---
+++ SPARCED/models/SPARCED_exp/amici_SPARCED_exp/SPARCED_exp_dxdotdx_explicit_rowvals.cpp
@@ -0,0 +1,14 @@
+#include "amici/sundials_matrix_wrapper.h"
+#include "sundials/sundials_types.h"
+
+#include <array>
+#include <algorithm>
+
+namespace amici {
+namespace model_SPARCED_exp {
+
+
+void dxdotdx_explicit_rowvals_SPARCED_exp(SUNMatrixWrapper &dxdotdx_explicit){
+}
+} // namespace amici
+} // namespace model_SPARCED_exp
