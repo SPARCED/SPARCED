b100fe47b7286ae3e39ee10707934eb3bbbae03f
chocolatecharlie <aurore.amrit@umu.se>
2024-12-12 15:39:15 +0100

    Create Experiment class

--- SPARCED/src/Experiment.py
+++ SPARCED/src/Experiment.py
@@ -5,6 +5,7 @@ import os
 
 import constants as const
 
+from utils.data_handling import load_configuration_file
 from utils.files_handling import *
 
 
@@ -13,20 +14,28 @@ from utils.files_handling import *
 class Experiment:
     def __init__(self,
                  name: str,
-                 experiments_directory: str | os.PathLike,
-                 simulation_files: dict[str, str],
-                 config_name: str
-                 ):
-        # General settings
-        self.name = name
-        self.path = append_subfolder(experiments_directory, self.name)
-        if not config_name:
-            config_name = self.name + const.EXPERIMENT_CONFIG_EXTENSION
-        self.configuration = self.load_configuration(self.path, config_name)
-        # TODO: add stamp to results folder
-        self.output_directory = append_subfolder(self.configuration[const.OUTPUT_DIRECTORY, self.name)
-        self.nb_replicates = self.configuration[const.NB_REPLICATES]
-        self.exchange = self.configuration[const.EXCHANGE]
-        self.verbose = self.configuration[const.VERBOSE]
-        self.simulation_files = simulation_files
-        # In the YAML config file too: model_name, amici_path, sbml_file
+                 experiements_directory: str | os.PathLike,
+                 model_name: str,
+                 amici_path: str | os.PathLike,
+                 sbml_path: str | os.PathLike,
+                 simulation_files: dict[str, str]):
+    # General settings
+    self.name = name
+    self.path = append_subfolder(experiments_directory, self.name)
+    self.configuration = self.load_configuration_file(self.path,
+                            self.name + const.DEFAULT_CONFIG_FILES_EXTENSION)
+    # External data
+    self.model_name = model_name
+    self.amici_path = amici_path
+    self.sbml_path = sbml_path
+    self.simulation_files = simulation_files
+    # Configuration unpacking
+    self.exchange
+    self.nb_replica = int(
+                      self.configuration[const.YAML_EXPERIMENT_NB_REPLICATES])
+    # TODO: Add stamp to output_directory name if necessary
+    self.output_directory = append_subfolder(self.configuration[
+                                    const.YAML_EXPERIMENT_OUTPUT_DIRECTORY,
+                                    self.name])
+    self.verbose = self.configuration[const.YAML_EXPERIMENT_VERBOSE]
+
