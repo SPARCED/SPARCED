76c9931a36399ea5323d6dd0af4ed215ebb64c52
chocolatecharlie <aurore.amrit@umu.se>
2024-12-13 11:38:51 +0100

    Sanitize nb replicates

--- SPARCED/src/Experiment.py
+++ SPARCED/src/Experiment.py
@@ -17,6 +17,18 @@ from utils.data_handling import *
 from utils.files_handling import *
 
 
+# CUSTOM ERRORS
+
+class NegativeNumberOfReplicates(ValueError):
+    def __init__(self, message: str, nb_replicates: int):
+        self.message = message
+        self.nb_replicates = int(nb_replicates)
+
+    def __str__(self):
+        return(f"SPARCED ERROR: Negative number of replicates.\n"
+             + f"Number of replicates: {self.nb_replicates}\n"
+             + f"Error: {self.message}\n")
+
 # EXPERIMENT
 
 class Experiment:
@@ -38,8 +50,8 @@ class Experiment:
         self.simulation_files = simulation_files
         # Configuration unpacking
         self.exchange
-        self.nb_replica = int(
-                      self.configuration[const.YAML_EXPERIMENT_NB_REPLICATES])
+        self.nb_replica = self.sanitize_nb_replicates(int(
+                    self.configuration[const.YAML_EXPERIMENT_NB_REPLICATES])
         # TODO: Add stamp to output_directory name if necessary
         self.output_directory = append_subfolder(self.configuration[
                                     const.YAML_EXPERIMENT_OUTPUT_DIRECTORY,
@@ -59,13 +71,34 @@ class Experiment:
         while cell_number <= self.nb_replica:
             for step in self.configuration[const.YAML_EXPERIMENT_PROTOCOL]:
                 protocol = step[1] # Skip name
-                simulation = SparcedSimulation(NAME, self.output_directory, DURATION, IS_DETERMINISTIC, cell_number, self.verbose)
+                print(protocol)
+                #simulation = SparcedSimulation(NAME, self.output_directory, DURATION, IS_DETERMINISTIC, cell_number, self.verbose)
 
             
             # Apply perturbations
             # Create a Simulation that matches
             # Run that simulation
             # Store concentrations levels
-        del simulation
+        #del simulation
         cell_number += 1
+
+    def sanitize_nb_replicates(self, nb_replicates: int) -> int:
+        """Sanitize number of replicates
+
+        Arguments:
+            nb_replicates: The number of replicates.
+
+        Returns:
+            The corresponding sanitized number of replicates.
+        """
+
+        try:
+            if int(nb_replicates) <= 0:
+                raise NegativeNumberOfReplicates(
+                                "Automatically setting to one replicate.\n",
+                                nb_replicates)
+        except NegativeNumberOfReplicates as error:
+            print(error)
+            nb_replicates = 1
+        return(nb_replicates)
 
