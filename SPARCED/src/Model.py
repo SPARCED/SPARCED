66901746777fec4a93f8916a7e2714ced4f6d300
chocolatecharlie <aurore.amrit@umu.se>
2024-12-17 12:01:35 +0100

    Ruff Model class

--- SPARCED/src/Model.py
+++ SPARCED/src/Model.py
@@ -7,28 +7,31 @@ import sys
 import numpy as np
 
 import constants as const
-
-from utils.data_handling import load_input_data_file, load_configuration_file
-from utils.files_handling import *
-
+from utils.data_handling import load_configuration_file, load_input_data_file
+from utils.files_handling import append_subfolder, check_path_existence
 
 # CUSTOM ERRORS
 
+
 class InvalidModelName(ValueError):
-    def __init__(self, messagei: str, model_name: str):
+    def __init__(self, message: str, model_name: str):
         self.message = message
         self.model_name = model_name
 
     def __str__(self):
-        return("SPARCED ERROR: Invalid model name.\n"
-             + f"Name: {self.model_name}\n"
-             + f"Error: {self.message}\n")
+        return (
+            "SPARCED ERROR: Invalid model name.\n"
+            + f"Name: {self.model_name}\n"
+            + f"Error: {self.message}\n"
+        )
+
 
 class EmptyModelName(InvalidModelName):
     pass
 
 
 # MODEL
+
 
 class Model:
     """Systems biology mathematical model definition as an object
@@ -59,51 +62,64 @@ class Model:
             validate_name()
     """
 
-    def __init__(self,
-                 name=const.DEFAULT_MODEL_NAME,
-                 models_directory=const.DEFAULT_MODELS_DIRECTORY,
-                 config_name=const.DEFAULT_CONFIG_FILE):
+    def __init__(
+        self,
+        name=const.DEFAULT_MODEL_NAME,
+        models_directory=const.DEFAULT_MODELS_DIRECTORY,
+        config_name=const.DEFAULT_CONFIG_FILE,
+    ):
         # General settings
         self.name = self.sanitize_name(name)
         self.path = append_subfolder(models_directory, self.name)
         self.configuration = load_configuration_file(self.path, config_name)
-        self.data_location = append_subfolder(self.path,
-                                self.configuration[const.YAML_DATA_LOCATION])
+        self.data_location = append_subfolder(
+            self.path, self.configuration[const.YAML_DATA_LOCATION]
+        )
         # Compilation
-        self.compilation_config = \
-                            self.configuration[const.YAML_COMPILATION_KEYWORD]
+        self.compilation_config = self.configuration[
+            const.YAML_COMPILATION_KEYWORD
+        ]
         self.compilation_data_path = append_subfolder(
-                self.data_location,
-                self.compilation_config[const.YAML_COMPILATION_DATA_LOCATION])
+            self.data_location,
+            self.compilation_config[const.YAML_COMPILATION_DATA_LOCATION],
+        )
         check_path_existence(self.compilation_data_path)
         self.compilation_files = self.load_files_path(
-                    self.compilation_data_path,
-                    self.compilation_config[const.YAML_COMPILATION_FILES])
+            self.compilation_data_path,
+            self.compilation_config[const.YAML_COMPILATION_FILES],
+        )
         self.compartments = self.load_compartments(
-                            self.compilation_files[const.YAML_COMPARTMENTS])
+            self.compilation_files[const.YAML_COMPARTMENTS]
+        )
         # Simulation (Optional)
         if const.YAML_SIMULATION_KEYWORD in self.configuration:
-            self.simulation_config = \
-                            self.configuration[const.YAML_SIMULATION_KEYWORD]
+            self.simulation_config = self.configuration[
+                const.YAML_SIMULATION_KEYWORD
+            ]
             self.simulation_data_path = append_subfolder(
                 self.data_location,
-                self.simulation_config[const.YAML_SIMULATION_DATA_LOCATION])
+                self.simulation_config[const.YAML_SIMULATION_DATA_LOCATION],
+            )
             check_path_existence(self.simulation_data_path)
             self.simulation_files = self.load_files_path(
-                        self.simulation_data_path,
-                        self.simulation_config[const.YAML_SIMULATION_FILES])
+                self.simulation_data_path,
+                self.simulation_config[const.YAML_SIMULATION_FILES],
+            )
         # Experiments (Optional)
         if const.YAML_EXPERIMENT_KEYWORD in self.configuration:
-            self.experiments_config = \
-                            self.configuration[const.YAML_EXPERIMENT_KEYWORD]
+            self.experiments_config = self.configuration[
+                const.YAML_EXPERIMENT_KEYWORD
+            ]
             self.experiments_data_path = append_subfolder(
                 self.data_location,
-                self.experiments_config[const.YAML_EXPERIMENT_DATA_LOCATION])
+                self.experiments_config[const.YAML_EXPERIMENT_DATA_LOCATION],
+            )
             check_path_existence(self.experiments_data_path)
             # Experiment name (Optional)
             if const.YAML_EXPERIMENT_NAME in self.experiments_config:
-                self.experiment_name = \
-                    self.experiments_config[const.YAML_EXPERIMENT_NAME]
+                self.experiment_name = self.experiments_config[
+                    const.YAML_EXPERIMENT_NAME
+                ]
 
     def load_compartments(self, path: str | os.PathLike) -> np.ndarray:
         """Load compartments from a PEtab file
@@ -126,10 +142,11 @@ class Model:
 
         check_path_existence(path)
         compartments = load_input_data_file(path)
-        return(compartments)
+        return compartments
 
-    def load_files_path(self, path: str | os.PathLike, files: dict[str, str]
-                        ) -> dict[str, str | os.PathLike]:
+    def load_files_path(
+        self, path: str | os.PathLike, files: dict[str, str]
+    ) -> dict[str, str | os.PathLike]:
         """Load input file paths
 
         Note:
@@ -149,16 +166,16 @@ class Model:
 
         # TODO: adjust for multiple files
         for file_type, file_name in files.items():
-            if file_name != None:
+            if file_name is not None:
                 files[file_type] = append_subfolder(path, file_name)
                 # Do not check existence of files that will be created upon
                 # compilation TODO: change to a list to handle several files
                 if file_type != const.YAML_OUTPUT_PARAMETERS:
                     check_path_existence(files[file_type])
-        return(files)
+        return files
 
     def sanitize_name(self, name: str) -> str:
-        """ Sanitize model name
+        """Sanitize model name
 
         Note:
             Model name cannot be empty.
@@ -180,13 +197,14 @@ class Model:
         try:
             if not name:
                 raise EmptyModelName("No model name provided.\n", name)
-            elif '-' in name:
-                raise InvalidModelName("Dash '-' character not supported.\n", name)
+            elif "-" in name:
+                raise InvalidModelName(
+                    "Dash '-' character not supported.\n", name
+                )
         except EmptyModelName as error:
             print(error)
             sys.exit(0)
         except InvalidModelName as error:
             print(error)
-            name = name.replace('-', '_')
-        return(name)
-
+            name = name.replace("-", "_")
+        return name
