d5633bced909cf429dc5f7e5da19220606e2dbfd
chocolatecharlie <aurore.amrit@umu.se>
2024-12-17 12:19:17 +0100

    Ruff main script

--- SPARCED/src/__main__.py
+++ SPARCED/src/__main__.py
@@ -8,14 +8,13 @@
 from compilation.compilation import create_and_compile_model
 from simulation.experiment import run_experiment
 
-# 1. Compile the model 
+# 1. Compile the model
 # You may also want to comment this section out and run a model that was
 # previously built instead.
 # Note that compiling the model may take several minutes.
-# model = create_and_compile_model()
+model = create_and_compile_model()
 
 # 2. Run an experiment
 # An experiment consists into one or several simulations involving one
 # or several cells.
 run_experiment()
-
