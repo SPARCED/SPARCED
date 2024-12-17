974870474b371a54e5b4ef456dc2bc513541ada3
chocolatecharlie <aurore.amrit@umu.se>
2024-12-17 11:44:53 +0100

    Ruff SBML to AMICI conversion script

--- SPARCED/src/compilation/conversion_scripts/sbml_to_amici.py
+++ SPARCED/src/compilation/conversion_scripts/sbml_to_amici.py
@@ -4,13 +4,15 @@
 import os
 
 import amici
-import libsbml
-
 from compilation.amici_scripts.creation import amici_create_folder
 
 
-def convert_sbml_to_amici(sbml_file_path: str, model_name: str, model_path: str | os.PathLike,
-                          verbose: bool) -> str | os.PathLike:
+def convert_sbml_to_amici(
+    sbml_file_path: str,
+    model_name: str,
+    model_path: str | os.PathLike,
+    verbose: bool,
+) -> str | os.PathLike:
     """Convert an SBML file into an AMICI model
 
     Note:
@@ -32,18 +34,21 @@ def convert_sbml_to_amici(sbml_file_path: str, model_name: str, model_path: str 
 
     amici_folder_path = amici_create_folder(model_name, model_path)
     importer = amici.SbmlImporter(sbml_file_path)
-    # Speed up compilation by setting all the rate parameters
-    # as constants
-    reader = libsbml.SBMLReader()
-    document = reader.readSBML(sbml_file_path)
-    sbml_model = document.getModel()
-    constant_parameters = [parameters.getId() \
-                           for parameters in sbml_model.getListOfParameters()]
-    # Compilation
+
+    # DISABLED: Speed up compilation by setting all the rate
+    # parameters as constants
+    #
+    # reader = libsbml.SBMLReader()
+    # document = reader.readSBML(sbml_file_path)
+    # sbml_model = document.getModel()
+    # constant_parameters = [parameters.getId() \
+    #                        for parameters in sbml_model.getListOfParameters()]
+
     importer.sbml2amici(model_name, amici_folder_path, verbose=verbose)
     if verbose:
-        print("SPARCED VERBOSE: Finished to convert SBML file of model " +
-              f"{model_name} to AMICI folder.\n")
+        print(
+            "SPARCED VERBOSE: Finished to convert SBML file of model "
+            + f"{model_name} to AMICI folder.\n"
+        )
 
-    return(amici_folder_path)
-
+    return amici_folder_path
