f88fe605ba34f73f6eda226714fe3a4ee0a21a6b
chocolatecharlie <aurore.amrit@umu.se>
2024-12-17 11:34:52 +0100

    Ruff Antimony to SBML connversion script

--- SPARCED/src/compilation/conversion_scripts/antimony_to_sbml.py
+++ SPARCED/src/compilation/conversion_scripts/antimony_to_sbml.py
@@ -4,15 +4,15 @@
 import os
 
 import antimony
-
-
 from compilation.sbml_scripts.creation import build_sbml_model_path
 
 
-def convert_antimony_to_sbml(antimony_file_path: str | os.PathLike,
-                             model_name: str,
-                             model_path: str | os.PathLike,
-                             verbose: bool) -> str | os.PathLike:
+def convert_antimony_to_sbml(
+    antimony_file_path: str | os.PathLike,
+    model_name: str,
+    model_path: str | os.PathLike,
+    verbose: bool,
+) -> str | os.PathLike:
     """Convert an Antimony file into an SBML file
 
     Note:
@@ -34,19 +34,25 @@ def convert_antimony_to_sbml(antimony_file_path: str | os.PathLike,
     if antimony.loadFile(str(antimony_file_path)) == -1:
         if verbose:
             print(antimony.getLastError())
-        raise RuntimeError("Failed to load Antimony file of model " +
-                           f"{model_name}.\n")
+        raise RuntimeError(
+            "Failed to load Antimony file of model " + f"{model_name}.\n"
+        )
     elif verbose:
-            print("SPARCED VERBOSE: Successfully loaded Antimony file of " +
-                  f"model {model_name}.\n")
+        print(
+            "SPARCED VERBOSE: Successfully loaded Antimony file of "
+            + f"model {model_name}.\n"
+        )
     # Convert the Antimony file into an SBML model
     if antimony.writeSBMLFile(str(sbml_file_path), model_name) == 0:
         if verbose:
             print(antimony.getLastError())
-        raise RuntimeError("Failed to convert Antimony file of model " +
-                           f"{model_name} to SBML format.\n")
+        raise RuntimeError(
+            "Failed to convert Antimony file of model "
+            + f"{model_name} to SBML format.\n"
+        )
     elif verbose:
-        print("SPARCED VERBOSE: Successfully converted Antimony file of " +
-              f"model {model_name} to SBML format.\n")
-    return(sbml_file_path)
-
+        print(
+            "SPARCED VERBOSE: Successfully converted Antimony file of "
+            + f"model {model_name} to SBML format.\n"
+        )
+    return sbml_file_path
